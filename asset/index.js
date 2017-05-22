5f71d39a44337867eefe81c68abc4570052379dc
wurui <wurui.wr@gmail.com>
2017-05-22 16:34:03 +0800

    first commit

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,143 @@
-define([],function(){
-  return {
-    init:function($mod){
+define(['zepto', './carlogo'], function (undef, Carlogo) {
+    /*
+     var cars=[];
+     $('ul.brand_ul').each(function(){
+     $(this).children('li').each(function(){
+     var img=$(this).children('a').children('img')[0],
+     span=$(this).children('a').children('span');
+     //console.log(img.src)
+     var mt=img.src.match(/(\d+)\.\w+$/i);
+     if(mt)
+     cars.push({
+     name:span.text(),
+     no:mt[1]
+     })
+     })
+     });*/
 
+
+
+
+    var pageIndex = 0,
+        $logoContainer;
+    var render = function () {
+        $logoContainer.html(Carlogo.render(pageIndex)).addClass('roll')
+        var images = $logoContainer.children('img');
+        var len = images.length;
+        var deg = Math.floor(360 / len);
+        for (var i = 0; i < images.length; i++) {
+            images[i].style.transform = 'rotateY(' + deg * i + 'deg) translateZ(160px)'; //deg前面不要加空格
+        }
     }
-  }
+    return {
+        init: function ($mod) {
+
+            var $bg = $('.J_bg', $mod);
+            var $editor = $('.J_editor', $mod);
+            var $coolthings = $('.J_coolthings', $mod);
+            var $central = $('.J_central', $mod).on('click', function () {
+                    $logoContainer.css('animationPlayState', "running").show()
+                    $editor.addClass('fadeout');
+                    $bg.removeClass('fadeout')
+                    $('img', $bg).remove();
+                    $coolthings.show().addClass('fadein')
+                    setTimeout(function () {
+                        $editor.hide();
+                    }, 1000)
+                }),
+                $text1 = $('.J_text1', $mod).on('click', function () {
+
+
+                }),$text2=$('.J_text2',$mod);
+
+            $logoContainer = $('.J_logo', $mod).on('click', function (e) {
+                var tar = e.target;
+                if (tar.tagName.toLowerCase() == 'img') {
+                    var no = tar.getAttribute('data-no'),
+                        name = tar.getAttribute('data-name');
+
+                    $logoContainer.css('animationPlayState', "paused");
+                    var cloneNode;
+                    $bg.prepend(cloneNode = $(tar).clone().removeAttr('style')).addClass('fadein');
+                    setTimeout(function () {
+                        $editor.show().removeClass('fadeout');
+                        $central.html(no ? '<img src="' + Carlogo.fullpath(no) + '"/>' : '').attr('data-no',no);
+                        $bg.removeClass('fadein').addClass('fadeout')
+                        $coolthings.addClass('fadeout')
+                        $logoContainer.hide();
+                        setTimeout(function () {
+                            $coolthings.hide();
+                        }, 300)
+                    }, 1500)
+                    //cloneNode.addClass('transition').css('transform','rotateY(0)');//.css('opacity',0).addClass('fasein')
+
+
+                    //alert('你选择了:'+name)
+                    //$logoContainer.addClass('fadeout')
+                }
+            });
+
+            render()
+
+
+            $mod.on('click', '.J_reset', function (e) {
+                var role = e.target.getAttribute('data-role')
+
+                switch (role) {
+                    case 'next':
+                        pageIndex++;
+                        render()
+                        break
+                    case 'prev':
+                        if (pageIndex > 0) {
+                            pageIndex--;
+                            render()
+                        }
+                        break
+                }
+
+
+            });
+
+            $($mod).on('submit',function(e){
+                var f= e.target;
+
+                var settings = {
+                    tpl: 1,
+                    bgcolor: 1,
+                    // color1:1
+                    text1: $text1.val(),
+                    text2:$text2.val(),
+                    carlogo:$central.attr('data-no')
+                };
+                //console.log(settings)
+
+                for(var k in settings){
+                    var hid= f[k];
+                    if(!hid){
+                        var hid=document.createElement('input');
+                        hid.type='hidden';
+                        hid.name=k;
+
+                        f.appendChild(hid)
+                    }
+                    hid.value=settings[k];
+                }
+                return false;
+
+
+
+            });
+
+
+        }
+    }
 })
+
+
+/**
+ * Created by wurui on 01/04/2017.
+ */
+
+
+
--- mod.xsl
+++ mod.xsl
@@ -1,10 +1,64 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oxm="https://www.openxsl.com">
     <xsl:template match="/root" name="wurui.smct-landing">
+        <xsl:param name="actionShopcart">http://www./smct/submitbuild</xsl:param>
+        <xsl:param name="actionWorks">/smct/submitbuild</xsl:param>
         <!-- className 'J_OXMod' required  -->
+        <!---
+        主要是搞点酷炫效果来吸引用户
+        -->
         <div class="J_OXMod oxmod-smct-landing" ox-mod="smct-landing">
-            <h1>
-                This is mod smct-landing;
-            </h1>
+            <div class="fullscreen J_coolthings">
+                <div class="background">
+
+                </div>
+                <div class="playground">
+                    <div class="logos-bg"></div>
+                    <div class="J_logo logos"></div>
+                    <h3 class="goldtitle">请选择你的车标</h3>
+                    <div class="bottom">
+                        <button class="J_reset" data-role="prev">« 前一批</button>
+                        <button class="J_reset" data-role="next">换一批 »</button>
+                    </div>
+                </div>
+                <div class="J_bg topmask">
+                    <h3 class="goldtitle">已选车标</h3><br/>
+                </div>
+            </div>
+
+            <div class="editor J_editor">
+                <h3 class="title">车贴定制</h3>
+                <div class="preview bgcolor-1">
+                    <div class="card-header">
+                        <input type="text" value="扫码移车" class="J_text1 maintext" maxLength="6"/>
+                    </div>
+                    <div class="card-body tpl tpl-1">
+                        <div class="central J_central"></div>
+                        <img src="http://i.oxm1.cc/uploads/git/wurui/img/2ahkwkkveTj1rgh0ueRlcquA5vz-1000.png"
+                             class="qrcode" width="120"/>
+                    </div>
+                    <div class="card-footer">
+                        <input type="text" class="subtext J_text2" value="www.shaomachetie.com" maxLength="25"/>
+                        <!--
+                        <span class="J_text2">
+
+                        </span>-->
+                    </div>
+                </div>
+
+                <div class="buttons">
+                    <form method="post" action="{$actionShopcart}">
+                        <button type="submit" class="bt-main">我很满意,提交!</button>
+<!--
+                        <button type="submit" onclick="this.form.action='{$actionWorks}'" class="bt-second">保存至我的车贴</button>
+                        -->
+                    </form>
+                </div>
+                <p>实物尺寸 72mm &#215; 72mm</p>
+                <br/><br/>
+
+            </div>
+
         </div>
+
     </xsl:template>
 </xsl:stylesheet>
